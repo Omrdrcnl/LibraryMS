f75beab2bbe6b64b543644074b784739e45ff62b
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-14 14:34:00 +0300

    Category_Contr_View

--- LibraryMS/Controllers/CategoryController.cs
+++ LibraryMS/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using LibraryMS.Models.Entity;
 using LibraryMS.Controllers;
+using System.Diagnostics.Contracts;
 
 namespace LibraryMS.Controllers
 {
@@ -18,11 +19,52 @@ namespace LibraryMS.Controllers
 
             return View(d);
         }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult AddCategory(tblCategory c)
         {
             db.tblCategory.Add(c);
             db.SaveChanges();
             return View();
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = db.tblCategory.Find(id);
+            db.tblCategory.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //[HttpGet]
+        //public ActionResult CallCategory()
+        //{      
+        //    return View();
+        //}
+        //[HttpPost]
+        public ActionResult CallCategory(int id)
+        {
+            var ctg = db.tblCategory.Find(id);
+
+            return View("CallCategory",ctg);
+        }
+
+
+        //[HttpGet]
+        //public ActionResult EditCategory()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        public ActionResult EditCategory(tblCategory c)
+        {
+            var ktg = db.tblCategory.Find(c.Id);
+            ktg.Name = c.Name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }

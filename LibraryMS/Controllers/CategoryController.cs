60533eeb819851616f52e1c50ca36edd46388edc
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-14 12:03:59 +0300

    Add project files.

---
+++ LibraryMS/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryMS.Models.Entity;
+using LibraryMS.Controllers;
+
+namespace LibraryMS.Controllers
+{
+    public class CategoryController : Controller
+    {
+        DbLibraryMSEntities db = new DbLibraryMSEntities();
+        // GET: Category
+        public ActionResult Index()
+        {
+            var d = db.tblCategory.ToList();
+
+            return View(d);
+        }
+        public ActionResult AddCategory(tblCategory c)
+        {
+            db.tblCategory.Add(c);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}

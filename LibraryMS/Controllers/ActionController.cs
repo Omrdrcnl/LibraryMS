8dbefb999c2fc7b2de48b64be7c93c60e437a516
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-19 21:17:07 +0300

    BookProcessStarting

---
+++ LibraryMS/Controllers/ActionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryMS.Controllers;
+using LibraryMS.Models.Entity;
+
+namespace LibraryMS.Controllers
+{
+    
+    public class ActionController : Controller
+    {
+        DbLibraryMSEntities db = new DbLibraryMSEntities();
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult LendBook()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LendBook(tblAction p)
+        {
+            
+            db.tblAction.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("LendBook");
+        }
+        public ActionResult BorrowBook()
+        {
+            return View();
+        }
+    }
+}
